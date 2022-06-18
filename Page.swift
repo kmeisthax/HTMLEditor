950a5b71cbf49aa6beddd8ecc738f36f2b32df8c
David Wendt <dcrkid@yahoo.com>
2022-06-18 14:57:12 -0600

    Add support for opening external files

--- Page.swift
+++ Page.swift
@@ -1,12 +1,74 @@
 import SwiftUI
 import Foundation
 
-class Page : ObservableObject, Identifiable {
+/**
+ * Viewmodel class for individual HTML files in a project.
+ * 
+ * Intended to be owned by a Project viewmodel. Also adopts NSFilePresenter
+ * to allow file updates (which apparently even Swift Playgrounds itself
+ * doesn't do?)
+ * 
+ * View code should not create pages directly; views should ask their
+ * associated Project to create a Page and then access it from there.
+ */
+class Page : NSObject, ObservableObject, Identifiable, NSFilePresenter {
     var id: UUID;
+    
+    @Published var presentedItemURL: URL?;
+    
+    var filename: String {
+        if let url = presentedItemURL {
+            return url.lastPathComponent
+        } else {
+            return "Untitled"
+        }
+    };
+    
+    lazy var presentedItemOperationQueue: OperationQueue = OperationQueue.main;
     
     @Published var html: String = "<!DOCTYPE html>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
     
-    init() {
+    override init() {
         id = UUID.init()
+    }
+    
+    class func fromSecurityScopedUrl(url: URL) -> Page {
+        let page = Page();
+        page.presentedItemURL = url;
+        
+        let coordinator = NSFileCoordinator.init(filePresenter: page);
+        
+        coordinator.coordinate(with: [.readingIntent(with: url)], queue: page.presentedItemOperationQueue) { error in
+            //TODO: Error handling.
+            if let error = error {
+                print (error);
+            }
+            
+            page.presentedItemDidChange();
+        };
+        
+        print("Opened");
+        
+        return page;
+    }
+    
+    func presentedItemDidChange() {
+        if let url = self.presentedItemURL {
+            if !CFURLStartAccessingSecurityScopedResource(url as CFURL) {
+                //panic! at the disco
+                print("Cannot access URL")
+            }
+            
+            do {
+                self.html = try String(contentsOf: url);
+            } catch {
+                //panic?!
+                print("Error reading URL")
+            }
+            
+            CFURLStopAccessingSecurityScopedResource(url as CFURL);
+        } else {
+            print("No URL")
+        }
     }
 }
--- Project.swift
+++ Project.swift
@@ -8,6 +8,9 @@ import SwiftUI
 class Project : NSObject, UIDocumentPickerDelegate, ObservableObject {
     @Published var openDocuments: [Page];
     
+    var lastSuccessCallback: (([URL]) -> Void)?;
+    var lastCancelCallback: (() -> Void)?;
+    
     override init() {
         self.openDocuments = []
     }
@@ -17,18 +20,43 @@ class Project : NSObject, UIDocumentPickerDelegate, ObservableObject {
     }
     
     func openPage(scene: UIWindowScene) {
+        openPage(scene: scene) { [self] urls in
+            for url in urls {
+                openDocuments.append(Page.fromSecurityScopedUrl(url: url))
+            }
+        }
+    }
+    
+    func openPage(scene: UIWindowScene, success: @escaping ([URL]) -> Void) {
+        openPage(scene: scene, success: success, cancel: nil);
+    }
+    
+    func openPage(scene: UIWindowScene, success: @escaping ([URL]) -> Void, cancel: (() -> Void)?) {
         let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.html]);
         
         documentPicker.delegate = self;
+        
+        self.lastSuccessCallback = success;
+        self.lastCancelCallback = cancel;
         
         scene.keyWindow?.rootViewController?.present(documentPicker, animated: true);
     }
     
     func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-        print("Picked.")
+        if let success = lastSuccessCallback {
+            success(urls)
+        }
+        
+        lastSuccessCallback = nil;
+        lastCancelCallback = nil;
     }
     
     func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-        print("Cancelled.")
+        if let cancel = lastCancelCallback {
+            cancel()
+        }
+        
+        lastSuccessCallback = nil;
+        lastCancelCallback = nil;
     }
 }
