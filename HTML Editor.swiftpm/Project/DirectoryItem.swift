9b19b55eb7c7b0f588ae5a9cdd07b0ec551ace4d
David Wendt <dcrkid@yahoo.com>
2022-07-05 08:22:01 -0600

    Add nonfunctional rename UI for files in the project listing

---
+++ HTML Editor.swiftpm/Project/DirectoryItem.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+struct DirectoryItem: View {
+    @Binding var entry: ProjectFileEntry;
+    
+    @State var isRenaming = false;
+    @State var renameTo = "";
+    
+    @FocusState var isRenameFocused;
+    
+    var body: some View {
+        if let contents = entry.contents {
+            NavigationLink(destination: PageEditor(page: contents)                   
+                .navigationTitle(contents.filename)
+                           #if os(iOS)
+                .navigationBarTitleDisplayMode(.inline)
+                           #endif
+            ) {
+                if isRenaming {
+                    HStack {
+                        Image(systemName: "doc.richtext")
+                        TextField("", text: $renameTo, onEditingChanged: { (isChanged) in
+                            if !isChanged {
+                                isRenaming = false;
+                                print(renameTo);
+                            }
+                        })
+                        .focused($isRenameFocused)
+                        .onChange(of: isRenameFocused) { newValue in 
+                            if !newValue {
+                                isRenaming = false;
+                            }
+                        }
+                    }
+                } else {
+                    Label(contents.filename, systemImage: "doc.richtext")
+                }
+            }
+            .contextMenu(ContextMenu {
+                Button {
+                    isRenaming = true;
+                    renameTo = contents.filename;
+                } label: {
+                    Label("Rename", systemImage: "pencil")
+                }
+            })
+        } else {
+            Label(entry.location.lastPathComponent, systemImage: "folder")
+        }
+    }
+}
--- HTML Editor.swiftpm/Project/DirectoryListing.swift
+++ HTML Editor.swiftpm/Project/DirectoryListing.swift
@@ -5,18 +5,7 @@ struct DirectoryListing: View {
     
     var body: some View {
         OutlineGroup($entries, children: \ProjectFileEntry.children) { $entry in
-            if let contents = entry.contents {
-                NavigationLink(destination: PageEditor(page: contents)                   
-                    .navigationTitle(contents.filename)
-                    #if os(iOS)
-                    .navigationBarTitleDisplayMode(.inline)
-                    #endif
-                    ) {
-                        Label(contents.filename, systemImage: "doc.richtext")
-                    }
-            } else {
-                Label(entry.location.lastPathComponent, systemImage: "folder")
-            }
+            DirectoryItem(entry: $entry)
         }
     }
 }
